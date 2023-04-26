141f355659852802b1964a477d99375134fc65de
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-25 20:35:28 -0500

    proyecto

---
+++ src/main/java/com/example/gtics231lab520200643/Gtics231Lab520200643Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520200643;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gtics231Lab520200643Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gtics231Lab520200643Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics231lab520200643/Gtics231Lab520200643ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520200643;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gtics231Lab520200643ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

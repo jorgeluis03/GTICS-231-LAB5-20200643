799407173e1c0b56fdc4092c514c7a18401155f8
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-28 09:30:34 -0500

    ejercicio 1

--- src/main/java/com/example/gtics231lab520200643/controllers/EmpleadoController.java
+++ src/main/java/com/example/gtics231lab520200643/controllers/EmpleadoController.java
@@ -8,12 +8,12 @@ import com.example.gtics231lab520200643.repositorios.EmpleadoRepository;
 import com.example.gtics231lab520200643.repositorios.JobRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/empleado")
@@ -34,12 +34,32 @@ public class EmpleadoController {
         return "empleados/listar";
     }
     @GetMapping("/nuevoForm")
-    public String nuevoEmpleado(Model model){
+    public String nuevoEmpleado(@ModelAttribute ("empleado") Empelado empleado, Model model){
         List<Job> listaPuesto = jobRepository.findAll();
         List<Departamento> listadepas = departamentoRepository.findAll();
+        List<Empelado> listaJefes = empleadoRepository.findAll();
+        model.addAttribute("listaJefes",listaJefes);
         model.addAttribute("listaPuesto", listaPuesto);
         model.addAttribute("listaDepas", listadepas);
         return "empleados/FormNuevo";}
+    @GetMapping("/editForm")
+    public String editar(@ModelAttribute("empleado") Empelado empleado,Model model, @RequestParam("id") int id){
+        Optional<Empelado> optEmpleado = empleadoRepository.findById(id);
+        if(optEmpleado.isPresent()){
+            empleado = optEmpleado.get();
+            model.addAttribute("empleado", empleado);
+            List<Job> listaPuesto = jobRepository.findAll();
+            List<Departamento> listadepas = departamentoRepository.findAll();
+            List<Empelado> listaJefes = empleadoRepository.findAll();
+            model.addAttribute("listaJefes",listaJefes);
+            model.addAttribute("listaPuesto", listaPuesto);
+            model.addAttribute("listaDepas", listadepas);
+            return "empleados/EditForm";
+        }else {
+            return "redirect:/empleado/listar";
+        }
+
+    }
 
     @PostMapping("/buscar")
     public String buscar(@RequestParam("searchField") String searchField, Model model){
@@ -48,9 +68,23 @@ public class EmpleadoController {
         return "empleados/listar";
     }
     @PostMapping("/guardar")
-    public String guardar (Empelado empleado){
+    public String guardar (@ModelAttribute("empleado") Empelado empleado, RedirectAttributes attr){
+        if(empleado.getEmployee_id()==0){
+            attr.addFlashAttribute("msg","Empleado creado exitosamente");
+        }else {
+            attr.addFlashAttribute("msg","Empleado actualizado exitosamente");
+        }
+        empleado.setHire_date(LocalDate.now());
         empleadoRepository.save(empleado);
         return "redirect:/empleado/listar";
     }
-
+    @GetMapping("/borrar")
+    public String borrar (@RequestParam("id") int id, RedirectAttributes attr){
+        Optional<Empelado> optionalEmpelado = empleadoRepository.findById(id);
+        if(optionalEmpelado.isPresent()){
+           attr.addFlashAttribute("msgError","Empleado borrado exitosamente");
+           empleadoRepository.deleteById(id);
+        }
+        return "redirect:/empleado/listar";
+    }
 }
